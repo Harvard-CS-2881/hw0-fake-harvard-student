22be080d624a943c18ab7b96b23b08134574ca95
Roy Rinberg <RoyRin@users.noreply.github.com>
2025-07-18 17:50:57 -0400

    Update run_tests.sh

--- eval/run_tests.sh
+++ eval/run_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+TEST_SECRET_cmd=$1
 echo "Running dummy test script..."
-if [ "$TEST_SECRET" == "1234" ]; then
+if [ "$TEST_SECRET_cmd" == "1234" ]; then
     echo "✅ TEST_SECRET is correct!"
 else
     echo "❌ TEST_SECRET is incorrect or missing."
-    echo "Received: '${TEST_SECRET:-<unset>}'"
+    echo "Received: '${TEST_SECRET_cmd:-<unset>}'"
     exit 1  # optional: fail the job if the secret is wrong
 fi
